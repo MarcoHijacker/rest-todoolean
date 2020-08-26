e3d42c070ac477aad71ff24fb255194bb3249b72
Marco Hijacker <marco.longo@hotmail.it>
2020-08-26 14:59:01 +0200

    Progetto iniziale vuoto.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Esercizio To-Do List JS
+
+// Area init
+
+function init() {
+
+}
+
+$(document).ready(init);
+
+// Area funzioni
+
+
+
+  // // Chiamata AJAX template
+  // $.ajax({
+  //   url: 'https://api.themoviedb.org/3/search/movie',
+  //   method: 'GET',
+  //   data: {
+  //     'api_key': 'd6c46515c238258071e0cac478212d4b',
+  //     'query': inputValue,
+  //     'language': 'it-IT'
+  //   },
+  //   success: function(data) {
+  //
+  //   },
+  //   error: function(err) {
+  //     console.log('err', err);
+  //   }
+  // });

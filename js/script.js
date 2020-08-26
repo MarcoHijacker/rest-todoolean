0ad475fa80f358d46b4a6e3f855263b476540719
Marco Hijacker <marco.longo@hotmail.it>
2020-08-26 16:50:12 +0200

    Versione definitiva.

--- js/script.js
+++ js/script.js
@@ -3,28 +3,77 @@
 // Area init
 
 function init() {
-
+  getTodoList();
+  addTodoBtnListener();
+  delTodoListener();
 }
 
 $(document).ready(init);
 
 // Area funzioni
 
+// Ottiene la lista dei ToDo tramite API
+function getTodoList() {
+  $.ajax({
+    url: 'http://157.230.17.132:3011/todos',
+    method: 'GET',
+    success: function(data) {
+      for (var i = 0; i < data.length; i++) {
+        $('#todolist').append(`<li class="item" data-id="${data[i].id}">${data[i].text} <i class="far fa-trash-alt"></i></li>`);
+      }
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+  });
+}
 
+// Listener sul click al bottone Add ToDo
+function addTodoBtnListener() {
+  $('#todobtn').click(addNewTodo);
+}
 
-  // // Chiamata AJAX template
-  // $.ajax({
-  //   url: 'https://api.themoviedb.org/3/search/movie',
-  //   method: 'GET',
-  //   data: {
-  //     'api_key': 'd6c46515c238258071e0cac478212d4b',
-  //     'query': inputValue,
-  //     'language': 'it-IT'
-  //   },
-  //   success: function(data) {
-  //
-  //   },
-  //   error: function(err) {
-  //     console.log('err', err);
-  //   }
-  // });
+// Aggiunge un nuovo ToDo prendendo l'input #todotext
+function addNewTodo() {
+  var input = $('#todotext');
+  var value = input.val();
+
+  $.ajax({
+    url: 'http://157.230.17.132:3011/todos',
+    method: 'POST',
+    data: {
+      'text': value
+    },
+    success: function(data) {
+      $('#todolist').text('');
+      input.val('');
+      getTodoList();
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+  });
+}
+
+// Listener sul click all'icona di cancellazione (trash)
+function delTodoListener() {
+  $(document).on('click', '.item', deleteTodo);
+}
+
+// Elimina un ToDo al click sul relativo logo Trash
+function deleteTodo() {
+  var item = $(this);
+  var todoid = item.data('id');
+
+  $.ajax({
+    url: `http://157.230.17.132:3011/todos/${todoid}`,
+    method: 'DELETE',
+    success: function(data) {
+      $('#todolist').text('');
+      getTodoList();
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+}
